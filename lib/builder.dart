b47714365a1a6dab1d371335f9b414f73e092ef1
Jacob MacDonald <jakemac@google.com>
2020-02-07 08:40:24 -0800

    simplify

--- lib/builder.dart
+++ lib/builder.dart
@@ -3,28 +3,32 @@ import 'dart:async';
 import 'package:build/build.dart';
 
 Builder create([BuilderOptions _]) => MyBuilder();
+Builder create2([BuilderOptions _]) => MyBuilder('.custom2.dart', 'Bar2');
 
 class MyBuilder extends Builder {
+  final String className;
+  final String outputExt;
+
+  MyBuilder([this.outputExt = '.custom.dart', this.className = 'Bar']);
+
   @override
   Future<void> build(BuildStep buildStep) async {
     final input = buildStep.inputId;
 
-    await buildStep.resolver.isLibrary(input);
+    final output = input.changeExtension(outputExt);
+    print('isLibrary: ${await buildStep.resolver.isLibrary(input)}');
 
     if (input.path.endsWith('input.dart')) {
-      final output = input.changeExtension('.custom.dart');
-      print('MyBuilder $input -> $output');
-
       await buildStep.writeAsString(output, '''
 part of 'input.dart';
 
-class Bar {}
+class $className {}
       ''');
     }
   }
 
   @override
-  Map<String, List<String>> get buildExtensions => const {
-        '.dart': ['.custom.dart']
+  Map<String, List<String>> get buildExtensions => {
+        '.dart': [outputExt]
       };
 }
